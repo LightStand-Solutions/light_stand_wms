33d0bf6b83bae6acdf4541796464d61a08d6526e
Mourishitz <gabrielmorishita@hotmail.com>
2023-02-24 00:46:23 -0300

    🎉 Initial Commit

---
+++ config/settings/test.py
@@ -0,0 +1,32 @@
+"""
+With these settings, tests run faster.
+"""
+
+from .base import *  # noqa
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="qN5E18LawXSnqeyJ81ikNfJxc0PDk19Ia6sw9i40KaRYnx5Ak5qybqYDHJroxRYp",
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
+
+# PASSWORDS
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+# DEBUGGING FOR TEMPLATES
+# ------------------------------------------------------------------------------
+TEMPLATES[0]["OPTIONS"]["debug"] = True  # type: ignore # noqa F405
+# Your stuff...
+# ------------------------------------------------------------------------------
---
+++ light_stand_wms/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from light_stand_wms.users.models import User
+from light_stand_wms.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
